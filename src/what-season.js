9af33043ab2cf745b06151dde2fe382df47a271e
Ismoil Ilxomov <ismoil6ek@gmail.com>
2022-05-01 15:44:50 +0500

    3tasks finish, commit to check if test works

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
+const { AssertionError } = require('chai');
 const { NotImplementedError } = require('../extensions/index.js');
+const seasonMonths = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'fall', 'fall', 'fall', 'winter'];
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,11 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+    if (Date.parse(date) === NaN) {
+        throw new AssertionError('Invalid date!');
+    } else return seasonMonths[date.getMonth()];
 }
 
 module.exports = {
-  getSeason
+    getSeason
 };
